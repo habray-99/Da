35039425bf7085703e2e9d51ecd6fa13ed99a10f
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-01-09 21:39:32 +0545

    added Orders.cs

---
+++ Components/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿namespace Draft.Components.Models;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Guid CoffeeId { get; set; }
+    public int Quantity { get; set; }
+    public List<Guid> AddOnIds { get; set; } = new List<Guid>();
+    public string CustomerPhone { get; set; }
+    public double TotalPrice { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public class AllOrders
+{
+    public List<Order> OrdersList { get; set; } = new List<Order>();
+}
