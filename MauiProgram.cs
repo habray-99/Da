3389d9e555d74e764b844884a443609e2e3f62a2
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-01-09 22:00:43 +0545

    added authentication

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Security.Claims;
+using Draft.Components.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
 
 namespace Draft
 {
@@ -15,6 +18,10 @@ namespace Draft
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+            
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
@@ -22,6 +29,41 @@ namespace Draft
 #endif
 
             return builder.Build();
+        }
+    }
+
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal _currentUser = new(new ClaimsIdentity());
+
+        public void SignIn(Users user)
+        {
+            if (user.Username != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new(ClaimTypes.Name, value: user.Username),
+                    // Add more claims if needed
+                };
+
+                claims.Add(user.IsAdmin ? new Claim(ClaimTypes.Role, "Admin") : new Claim(ClaimTypes.Role, "Staff"));
+
+                var identity = new ClaimsIdentity(claims, "Custom");
+                _currentUser = new ClaimsPrincipal(identity);
+            }
+
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(_currentUser));
+        }
+
+        public void SignOut()
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
         }
     }
 }
