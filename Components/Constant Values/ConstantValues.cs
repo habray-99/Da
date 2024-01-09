bf9094b61a500bd0d63badaa76e29b909cd7b8d6
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-01-09 21:07:04 +0545

    updated bug

--- Components/Constant Values/ConstantValues.cs
+++ Components/Constant Values/ConstantValues.cs
@@ -2,10 +2,10 @@
 
 public class ConstantValues
 {
-    public static readonly string FilePath = "D:\\Cafe";
+    private const string FilePath = "D:\\Cafe";
     public static readonly string UserPath = Path.Combine(FilePath, "Users.json");
     public static readonly string CoffeePath = Path.Combine(FilePath, "Coffees.json");
     public static readonly string AddOnsPath = Path.Combine(FilePath, "AddOns.json");
-    public static readonly string CustomerPath = Path.Combine(FilePath, "Users.json");
+    public static readonly string CustomerPath = Path.Combine(FilePath, "Customers.json");
     public static readonly string OrdersPath = Path.Combine(FilePath, "Orders.json");
 }
