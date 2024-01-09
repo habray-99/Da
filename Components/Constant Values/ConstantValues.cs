cdd14e184d8ea7c767ed4f276fc58acdcd8baae5
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-01-09 20:56:12 +0545

    addded paths

---
+++ Components/Constant Values/ConstantValues.cs
@@ -0,0 +1,11 @@
+﻿namespace Draft.Components.Constant_Values;
+
+public class ConstantValues
+{
+    public static readonly string FilePath = "D:\\Cafe";
+    public static readonly string UserPath = Path.Combine(FilePath, "Users.json");
+    public static readonly string CoffeePath = Path.Combine(FilePath, "Coffees.json");
+    public static readonly string AddOnsPath = Path.Combine(FilePath, "AddOns.json");
+    public static readonly string CustomerPath = Path.Combine(FilePath, "Users.json");
+    public static readonly string OrdersPath = Path.Combine(FilePath, "Orders.json");
+}
