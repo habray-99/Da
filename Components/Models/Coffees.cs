881af4addab7cf09fabe28e374fca10a4628a181
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-01-11 12:09:12 +0545

    ya it fuking works

--- Components/Models/Coffees.cs
+++ Components/Models/Coffees.cs
@@ -3,7 +3,7 @@
 public class Coffees
 {
     public Guid Guid { get; set; }
-    public string? CoffeeName { get; set; }
+    public string CoffeeName { get; set; }
     public double Price { get; set; }
     // public int Quantity { get; set; }
     
--- Components/Models/Orders.cs
+++ Components/Models/Orders.cs
@@ -3,12 +3,18 @@
 public class Order
 {
     public Guid Id { get; set; }
-    public Guid CoffeeId { get; set; }
-    public int Quantity { get; set; }
-    public List<Guid> AddOnIds { get; set; } = new List<Guid>();
     public string CustomerPhone { get; set; }
     public double TotalPrice { get; set; }
     public DateTime CreatedAt { get; set; }
+    public List<OrderItem> OrderedItems { get; set; }
+}
+
+public class OrderItem
+{
+    public Guid CoffeeGuid { get; set; }
+    public List<Guid> AddIns { get; set; } = new List<Guid>();
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
 }
 
 public class AllOrders
